f98974d6283c82a03949584820efff65af4120c6
Pinku <pinkuparmer96@gmail.com>
2024-10-03 20:57:09 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import { useState } from 'react'
+// import reactLogo from './assets/react.svg'
+// import viteLogo from '/vite.svg'
+import GroceryInput from './components/GroceryInput'
+import './App.css'
+function App() {
+
+  return (
+    <>
+    <GroceryInput/>
+    </>
+  )
+}
+
+export default App
